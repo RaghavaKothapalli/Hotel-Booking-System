8a2043d2c37819ded6ed308929a72aac5ae3de6c
Raghava Sri Vastav <kothapalliraghava2002@gmail.com>
2024-11-04 22:22:36 +0530

    changed

--- src/components/Layouts.tsx
+++ src/components/Layouts.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
+import Image from "next/legacy/image"
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
